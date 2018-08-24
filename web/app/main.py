0b9d6a179f7c37bf25c0b5b7200a3001d34b04cc
Peter Krantz <peter.krantz@gmail.com>
2018-08-24 21:15:46 +0200

    tweaks + caching

--- web/app/main.py
+++ web/app/main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 from models import db, Ad
 from sqlalchemy.orm import load_only
 import os
+from flask_caching import Cache
+import time
 
 app = Flask(__name__)
 db.init_app(app)
@@ -16,6 +18,15 @@ POSTGRES = {
 }
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+cache = Cache(app, config={'CACHE_TYPE': 'memcached', 'CACHE_MEMCACHED_SERVERS': ('memcached:11211',)})
+
+def view_full_path_key():
+    return request.full_path
+
+
+@cache.cached(timeout=120, key_prefix='advertisers')
+def get_advertisers():
+    return [ad.advertiser for ad in Ad.query.options(load_only("advertiser")).order_by(Ad.advertiser).distinct("advertiser").all() if ad.advertiser is not None]
 
 
 @app.route("/", methods=['GET'])
@@ -23,16 +34,16 @@ def index():
 
     advertiser_filter = request.args.get('advertiser', None)
 
-    advertisers = [ad.advertiser for ad in Ad.query.options(load_only("advertiser")).order_by(Ad.advertiser).distinct("advertiser").all() if ad.advertiser is not None]
-
     ads = Ad.query.all()
     if advertiser_filter:
         ads = Ad.query.filter_by(advertiser=advertiser_filter)
 
-    return render_template('index.html', ads=ads, advertisers=advertisers, advertiser_filter=advertiser_filter)
+    return render_template('index.html', ads=ads, advertisers=get_advertisers(), advertiser_filter=advertiser_filter)
+
 
 
 @app.route("/ad/<string:fbid>", methods=['GET'])
+@cache.cached(timeout=60*60*24)
 def ad(fbid):
     ad = Ad.query.filter_by(fbid=fbid).first() 
 
@@ -40,6 +51,15 @@ def ad(fbid):
         return render_template('ad.html', ad=ad)
     else:
         abort(404)
+
+
+
+@app.route("/om-insamlingen", methods=['GET'])
+@cache.cached(timeout=60*60*24)
+def about():
+    return render_template('about.html')
+
+
 
 if __name__ == "__main__":
     # Only for debugging while developing
